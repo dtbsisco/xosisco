d39943578afd86f849fb647c96d633bc5361b7a5
dtbsisco <dasiscow@gmail.com>
2024-12-03 12:19:10 +0100

    update 10 fix

--- modal.js
+++ modal.js
@@ -1,20 +1,24 @@
 var modal = document.getElementById("imageModal");
 var modalImage = document.getElementById("modalImage");
-var closeBtn = document.getElementsByClassName("close")[0];
+var closeBtn = document.querySelector(".close");
+
+document.addEventListener("DOMContentLoaded", function() {
+    modal.style.display = "none";
+});
 
 document.querySelectorAll('.gallery-item img').forEach(function(img) {
     img.addEventListener('click', function() {
         modal.style.display = "flex";
-        modalImage.src = this.src; 
+        modalImage.src = this.src;
     });
 });
 
-closeBtn.onclick = function() {
+closeBtn.addEventListener('click', function() {
     modal.style.display = "none";
-}
+});
 
-window.onclick = function(event) {
-    if (event.target == modal) {
+window.addEventListener('click', function(event) {
+    if (event.target === modal) {
         modal.style.display = "none";
     }
-}
+});
